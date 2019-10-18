3875bc80924027c8526ec62e5352eaa623e5a5e5
Zeevj <mushmu@gmail.com>
2019-10-19 01:20:51 +0300

    more refactoring of the player

--- multiWeight/python/player.py
+++ multiWeight/python/player.py
@@ -1,65 +1,73 @@
 import pygame
 import yaml
 from time import sleep
-pygame.init()
-pygame.mixer.init(48000, -16, 1, 1024)
+import glob
 
+sounds = [pygame.mixer.Sound] * 8
 
 def parse(key, data):
     print("--->>>",data)
-    if ".wav" in data or ".mp3" in data:
-        # pygame.mixer.music.set_endevent("SONG_END")
-        file = "./song/"+data
-        print("opening ", file)
-        pygame.mixer.music.load(file)
-        # crash_sound = pygame.mixer.Sound(file)
-        # pygame.mixer.Sound.play(crash_sound)
+    
     if "play" in data:
         print("found play")
         pygame.mixer.music.play()
-    if "stop" in data:
+        # pygame.mixer.Sound.play(sounds[0])
+    elif "stop" in data:
         print("found stop")
         pygame.mixer.music.stop()
-    if key is not None and "step" in key:
+        # pygame.mixer.Sound.stop(sounds[0])
+    elif key is not None and "step" in key:
         if "ef" in data:
             print("step",key[4:],"will trigger effect", data[2:], "when pressed")
+    elif "ef" in data:
+        print("run effect", data[2:])
+    elif ".wav" in data or ".mp3" in data:
+        file = "./songs/"+data
+        print("opening ", file)
+        music = pygame.mixer.music.load(file)
+        # sounds[0] = pygame.mixer.Sound(file)
+        pygame.mixer.music.play()
+        pygame.mixer.music.stop()
 
-queue = {}
-timeCounterMs = 0
-tickMs = 100
+def main():
+    pygame.init()
+    pygame.mixer.init(48000, -16, 1, 1024)
 
-with open(loc + "/song/grav1.yaml", 'r') as stream:
-    try:
-        queue = yaml.safe_load(stream)
-        # print()
+    queue = {}
+    timeCounterMs = 0
+    tickMs = 100
 
-        keysToReplace = []
-        for key in queue:
-            if isinstance(key, (int, float, complex)):
-                keysToReplace.append(key)
-        
-        for key in keysToReplace:
-            queue[int(key*1000)] = queue.pop(key)
+    yamlList = glob.glob("./songs/*.yaml")
 
+    with open(yamlList[0], 'r') as stream:
+        try:
+            queue = yaml.safe_load(stream)
+            # print()
 
-                
-                # print(val)
-    except yaml.YAMLError as exc:
-        print(exc)
+            keysToReplace = []
+            for key in queue:
+                if isinstance(key, (int, float, complex)):
+                    keysToReplace.append(key)
+            for key in keysToReplace:
+                queue[int(key*1000)] = queue.pop(key)
+                    # print(val)
+        except yaml.YAMLError as exc:
+            print(exc)
 
-while timeCounterMs < 10 * 1000:
-    
-    timeKey = queue.pop(timeCounterMs, None)
-    # print("timeKey",timeCounterMs)
-    if timeKey != None:
-        if isinstance(timeKey,dict):
-            for key in timeKey:
-                # print(timeKey.keys())
-                parse(key, timeKey[key])
-        else:
-            parse(None, timeKey)
-    timeCounterMs = int(timeCounterMs + tickMs)
-    sleep(tickMs / 1000.0) 
-    
+    while len(queue) > 0:
+        timeKey = queue.pop(timeCounterMs, None)
+        # print("timeKey",timeCounterMs)
+        if timeKey != None:
+            if isinstance(timeKey,dict):
+                for key in timeKey:
+                    # print(timeKey.keys())
+                    parse(key, timeKey[key])
+            else:
+                parse(None, timeKey)
+        timeCounterMs = int(timeCounterMs + tickMs)
+        sleep(tickMs / 1000.0) 
+
+if __name__== "__main__":
+      main()
 
 
