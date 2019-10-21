ab07da16407a00357326d6a50a1f5105bf7c3f5b
Zeevj <mushmu@gmail.com>
2019-10-21 23:37:23 +0300

    test weight commit

--- testWeight/testWeight.ino
+++ testWeight/testWeight.ino
@@ -4,7 +4,7 @@
 #include "HX711-multi.h"
 #define CLK 25      // clock pin to the load cell amp
 #define TARE_TIMEOUT_SECONDS 4
-byte DOUTS[] = {18}; //data from each pressure amplifier 
+byte DOUTS[] = {18}; //data from each pressure amplifier
 #define CHANNEL_COUNT sizeof(DOUTS)/sizeof(byte)
 long int results[CHANNEL_COUNT];
 HX711MULTI scales(CHANNEL_COUNT, DOUTS, CLK);
@@ -15,31 +15,110 @@ HX711MULTI scales(CHANNEL_COUNT, DOUTS, CLK);
 #define DATA_PIN 5
 CRGB leds[NUM_LEDS];
 
+int led = 2;
+
 struct Step {
   int fromLed;
   int toLed;
+  int highThreshold = 4000;
+  int lowThreshold = -4000;
+  int histeressis = 2000;
+  bool stepDetected;
+  bool stepsCounter;
 };
 
 Step steps[CHANNEL_COUNT];
 
 
 //================== Step Detection =============
-int highThreshold = 4000;
-int lowThreshold = -4000;
-int histeressis = 2000;
+//int highThreshold = 4000;
+//int lowThreshold = -4000;
+//int histeressis = 2000;
 
 bool stepsDetected[CHANNEL_COUNT];
-bool stepsCounter[CHANNEL_COUNT];
+int stepsValue[CHANNEL_COUNT];
+int stepsAvgValue[CHANNEL_COUNT];
+int stepsCounter[CHANNEL_COUNT];
 const int stepModes = 3;
+
+
+
+bool isNewValWayOverUnder() {
+  //
+}
+
+void printBorders(int i) {
+      Serial.print( stepsValue[i] );
+      Serial.print( "\t" );
+      Serial.print(steps[i].highThreshold );
+      Serial.print( "\t" );
+      Serial.print(steps[i].highThreshold - steps[i].histeressis );
+      Serial.print( "\t" );
+      Serial.print(steps[i].lowThreshold );
+      Serial.print( "\t" );
+      Serial.print(stepsAvgValue[i]);
+      Serial.print( "\t" );
+      Serial.println(steps[i].lowThreshold + steps[i].histeressis );
+
+      
+}
+
+
+void calibrateThresholds()
+{
+  for (int j = 0; j < scales.get_count(); j++)
+  {
+    steps[j].highThreshold = 2000;
+    steps[j].lowThreshold = -2000;
+  }
+
+  const int numberOfSamples = 80 * 4;
+  const float extraThresh = 0.1;
+
+  for (int i = 0; i < numberOfSamples; i++)
+  {
+    scales.read(results);
+    for (int j = 0; j < scales.get_count(); j++)
+    {
+      int scaleResult = -results[j];
+
+      if (steps[j].highThreshold < scaleResult)
+      {
+        steps[j].highThreshold = scaleResult + (scaleResult * extraThresh);
+      }
+
+      if (steps[j].lowThreshold > scaleResult)
+      {
+        steps[j].lowThreshold = scaleResult - (scaleResult * extraThresh);
+      }
+      steps[j].histeressis = abs(steps[j].highThreshold - steps[j].lowThreshold) * 0.25;
+
+      printBorders(j);
+    }
+
+    delay(1000 / 80);
+  }
+
+//  Serial.print("hight threshold: ");
+//  Serial.println(steps[0].highThreshold);
+//  Serial.print("low threhsold: ");
+//  Serial.println(steps[0].lowThreshold);
+//  Serial.print("histeressis: ");
+//  Serial.println(steps[0].histeressis);
+
+  
+}
+
 
 void setup() {
 
+  pinMode(led, OUTPUT);
+  digitalWrite(led, HIGH);
+  steps[0].fromLed = 0;
+  steps[0].toLed = 124;
 
-  steps[1].fromLed = 11;
-  steps[1].toLed = 150;
-
-  steps[0].fromLed = 160;
-  steps[0].toLed = 220;
+  //  steps[0].fromLed = 160;
+  //  steps[0].toLed = 220;
 
   Serial.begin(115200);
   Serial.flush();
@@ -49,6 +128,8 @@ void setup() {
   FastLED.clear();
   FastLED.show();
   tare();
+  calibrateThresholds();
+  digitalWrite(led, LOW);
 }
 
 void tare() {
@@ -61,30 +142,45 @@ void tare() {
 
 void detectSteps() {
   for (int i = 0; i < scales.get_count(); ++i) {
-    int scaleResult = -results[i];
+
+    float scaleResult = float(-results[i]);
+    float lerpVal = 0.6;
+    if (abs(scaleResult - stepsValue[i]) > 10000) {
+      lerpVal = 0.99;
+    }
+//    Serial.println(abs(scaleResult - stepsValue[i]) );
+    stepsValue[i] = stepsValue[i] * lerpVal + scaleResult * (1 - lerpVal);
+
+    float avgLerp = 0.999;
+    stepsAvgValue[i] = stepsAvgValue[i] * avgLerp + stepsValue[i] * (1-avgLerp);
+
     if (!stepsDetected[i]) {
-      if (scaleResult > highThreshold || scaleResult < lowThreshold) {
+      if (stepsValue[i] > steps[i].highThreshold || stepsValue[i] < steps[i].lowThreshold   ) {
         stepsDetected[i] = true;
         stepsCounter[i]++;
       }
     } else {
-      if (scaleResult < highThreshold - histeressis && scaleResult > lowThreshold + histeressis ) {
+      if (stepsValue[i] < (steps[i].highThreshold - (steps[i].histeressis ) ) && stepsValue[i] > (steps[i].lowThreshold + (steps[i].histeressis  )) ) {
         stepsDetected[i] = false;
       }
     }
-//
-    Serial.print( scaleResult + i * 20);
-    Serial.print( (i != scales.get_count() - 1) ? "\t" : "\n");
+    
+
+    //
+//    Serial.print( stepsValue[i] + i * 20);
+       printBorders(i);
+    
+//    Serial.print( (i != scales.get_count() - 1) ? "\t" : "\n");
   }
 }
 
 void renderLeds() {
-  for (int i = 0; i < scales.get_count(); ++i) {    
+  for (int i = 0; i < scales.get_count(); ++i) {
     if (stepsDetected[i]) {
       CRGB cl;
       int stp = stepsCounter[i] % stepModes;
 
-      switch(stp) {
+      switch (stp) {
         case 0:
           cl = CRGB::Red;
           break;
@@ -95,7 +191,7 @@ void renderLeds() {
           cl = CRGB::Blue;
           break;
       }
-      
+
       for (int j = steps[i].fromLed; j < steps[i].toLed; j++) {
         leds[j] = cl;
       }
