a5bf15ec3e0f2a74697aee8709d37757cfb37401
Zeevj <mushmu@gmail.com>
2019-10-25 11:19:58 +0300

    player and yaml in new format

--- multiWeight/python/player.py
+++ multiWeight/python/player.py
@@ -3,6 +3,7 @@ import yaml
 from time import sleep
 import glob
 import threading
+from colour import Color
 
 import test_serial as myserial
 
@@ -21,26 +22,80 @@ def play_song_num(num):
 
 def parse(key, data):
     print("--->>>",data)
+
+    data_tokens = [item.strip() for item in data.split(',')]
+
+    if len(data_tokens) == 1:
+        #this is maybe a spacial word like stop
+        pass
+
+    send_data = False
+    effect = -1
+    time = 100
+    red = 0
+    green = 0
+    blue = 0
+    turn_on = 1
+
+    for tok in data_tokens:
+        # if k is not None and "step" in k:
+        #     dt = [item.strip() for item in data.split(',')]
+        #     if "ef" in data:
+        #     print("step",k[4:],"will trigger effect", data[2:], "when pressed")
+        # if ".wav" in data or ".mp3" in data:
+        #     file = "./songs/"+data
+        #     sounds[int(k[4:])] = pygame.mixer.Sound(file)
+        # if "nosound" in data:
+        #     pygame.mixer.Sound.stop(sounds[int(k[4:])])
+        #     sounds[int(k[4:])] = pygame.mixer.Sound
+
+        first_char = tok[0]
+        the_rest = tok[1:]
+
+        if "e" == first_char:
+            effect = int(the_rest)
+            send_data = True
+        elif "t" == first_char:
+            time = int(the_rest)
+        elif "c" == first_char:
+            #pip3 install colour
+            color = Color(the_rest)
+            red = int(color.red*255)
+            green = int(color.green*255)
+            blue = int(color.blue*255)
+        elif "r" == first_char:
+            red = int(the_rest)
+        elif "g" == first_char:
+            green = int(the_rest)
+        elif "b" == first_char:
+            blue = int(the_rest)
+        elif "o" == first_char:
+            turn_on = int(the_rest)
+    if send_data:
+        cmd = "ef," + str(effect) + "," + str(turn_on) + "," + str(time) + "," + str(red) + "," + str(green) + "," + str(blue) + "\n"
+        print("sending ", cmd)
+        myserial.write(cmd.encode())
     
-    if "play" in data:
+    if "!play" in data:
         print("found play")
         pygame.mixer.music.play()
         # pygame.mixer.Sound.play(sounds[0])
-    elif "stop" in data:
+    elif "!stop" in data:
         print("found stop")
         pygame.mixer.music.stop()
         # pygame.mixer.Sound.stop(sounds[0])
-    elif key is not None and "step" in key:
-        if "ef" in data:
-            print("step",key[4:],"will trigger effect", data[2:], "when pressed")
-        if ".wav" in data or ".mp3" in data:
-            file = "./songs/"+data
-            sounds[int(key[4:])] = pygame.mixer.Sound(file)
-        if "nosound" in data:
-            pygame.mixer.Sound.stop(sounds[int(key[4:])])
-            sounds[int(key[4:])] = pygame.mixer.Sound
+    # elif key is not None and "step" in key:
+    #     if "ef" in data:
+    #         print("step",key[4:],"will trigger effect", data[2:], "when pressed")
+    #     if ".wav" in data or ".mp3" in data:
+    #         file = "./songs/"+data
+    #         sounds[int(key[4:])] = pygame.mixer.Sound(file)
+    #     if "nosound" in data:
+    #         pygame.mixer.Sound.stop(sounds[int(key[4:])])
+    #         sounds[int(key[4:])] = pygame.mixer.Sound
+    """
     elif "ef" in data:
-        tokens = data.split(",")
+        tokens = data_tokens
         tokenslen = len(tokens)
         if tokenslen > 3:
             time = tokens[2]
@@ -76,7 +131,9 @@ def parse(key, data):
         cmd = "ef," + str(effect_number) + "," + str(turn_on) + "," + str(time) + "," + str(red) + "," + str(green) + "," + str(blue) + "\n"
         print("sending ", cmd)
         myserial.write(cmd.encode())
-    elif ".wav" in data or ".mp3" in data:
+    """
+    # elif ".wav" in data or ".mp3" in data:
+    if ".wav" in data or ".mp3" in data:
         file = "./songs/"+data
         print("opening ", file)
         music = pygame.mixer.music.load(file)
